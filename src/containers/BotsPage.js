69c5b5b6e29521e28e06753b782fb78bd071b10c
tylergreason <tyler.greason@protonmail.com>
2020-03-18 10:34:09 -0400

    checkpoint

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,43 @@
 import React from "react";
+import YourBotArmy from "./YourBotArmy"
+import BotCollection from "./BotCollection"
+
+
+const API = "https://bot-battler-api.herokuapp.com/api/v1/bots"
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  state = {
+    bots:[],
+    botArmy:[]
+  }
+
+  componentDidMount = () => {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(data => this.setState({bots:data}))
+  }
+
+  // 3. Enlisting and Discharging bots
+  botCollectionClick = (bot) => {
+    (this.state.botArmy.filter(filterBot => filterBot.id === bot.id).length < 1)
+    ?
+    this.setState({botArmy:[...this.state.botArmy,bot]})
+    :
+    null
+  }
+
+  botArmyClick = (bot) =>{
+    console.log(bot)
+    const newBots = this.state.botArmy.filter(filterBot => filterBot.id !== bot.id)
+    this.setState({botArmy:newBots})
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        {<YourBotArmy bots={this.state.botArmy} botClick={this.botArmyClick}/>}
+        {<BotCollection bots={this.state.bots} botClick={this.botCollectionClick}/>}
       </div>
     );
   }
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -2,15 +2,18 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class YourBotArmy extends React.Component {
-  //your bot army code here...
+
+  displayBots = (bots) => {
+		return bots.map(bot => <BotCard bot={bot} botClick={this.props.botClick} key={bot.id}/>)
+	}
 
   render(){
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
             Your Bot Army
+            {this.displayBots(this.props.bots)}
           </div>
         </div>
       </div>
